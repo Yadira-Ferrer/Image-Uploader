7c0db226d72ad2d866aea2e0f42a0a740a25be51
yadiraferrer <yyfm.95@gmail.com>
2022-07-22 00:49:32 -0600

    Configuration of PrimeNg

--- imageUploader/src/app/app.module.ts
+++ imageUploader/src/app/app.module.ts
@@ -4,15 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/* Primeng */
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, PrimeNgModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
